12e8d14ff6055fc529d4c004649397be5aaa4033
Karl Webster <19327709+ktasper@users.noreply.github.com>
2024-04-19 19:07:12 +0100

    Test

--- app/main.py
+++ app/main.py
@@ -4,7 +4,8 @@ import socket
 def main():
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    client = server_socket.accept()[1] # wait for client
+    print (client)
 
 
 if __name__ == "__main__":
