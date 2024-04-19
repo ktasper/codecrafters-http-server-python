8d1379be157c1a9e590d4e3161e73b7ad764c689
Karl Webster <19327709+ktasper@users.noreply.github.com>
2024-04-19 20:57:36 +0100

    Test

--- app/main.py
+++ app/main.py
@@ -14,12 +14,17 @@ def main():
         print (f"path: {path}")
         path_vals: list[bytes] = path.split(b"/")
         print (f"path vals: {path_vals}")
+
         if path == b"/":
             conn.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+
         elif b"echo" in path_vals[1]:
             content = f"{path_vals[2].decode()}/{path_vals[3].decode()}"
             print (f"content: {content}")
             conn.send(f"HTTP/1.1 200 Ok\r\nContent-Type: text/plain\r\nContent-Length:{len(content)}\r\n\r\n{content}".encode())
+
+        elif b"user-agent" in path_vals[1]:
+            print("Hit user agent")
         else:
             conn.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
 
