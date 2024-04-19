dfa55b9c87763822df4827acd5d090a7f1afdafc
Karl Webster <19327709+ktasper@users.noreply.github.com>
2024-04-19 21:23:56 +0100

    Test

--- app/main.py
+++ app/main.py
@@ -1,52 +1,55 @@
 import socket
 import threading
 
-
-def main():
-    threads = []
-    thread = threading.Thread(target=worker)
-    threads.append(thread)
-    thread.start()
-    thread.join()
-
-def worker():
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    """Worker thread"""
-    print ("Starting worker thread")
-    server(server_socket)
-
+HOST = "localhost"
+PORT = 4221
 
 def server(server_socket):
-        conn, _ = server_socket.accept()
-        with conn:
-            print ("connected")
-            data = conn.recv(1024)
-            data = data.split(b" ")
-            print (f"Data: {data}")
-            path = data[1]
-            print (f"path: {path}")
-            path_vals: list[bytes] = path.split(b"/")
-            print (f"path vals: {path_vals}")
+  """Listens for connections and creates threads to handle them."""
+  while True:
+    conn, addr = server_socket.accept()
+    # Create a new thread to handle the client connection
+    client_thread = threading.Thread(target=handle_client, args=(conn, addr))
+    client_thread.start()
 
-            if path == b"/":
-                conn.send("HTTP/1.1 200 OK\r\n\r\n".encode())
 
-            elif b"echo" in path_vals[1]:
-                content = f"{path_vals[2].decode()}/{path_vals[3].decode()}"
-                print (f"content: {content}")
-                conn.send(f"HTTP/1.1 200 Ok\r\nContent-Type: text/plain\r\nContent-Length:{len(content)}\r\n\r\n{content}".encode())
+def handle_client(conn, addr):
+  """Handles a single client connection."""
+  print(f"Connected by {addr}")
+  with conn:
+    try:
+      data = conn.recv(1024)
+      data = data.split(b" ")
+      print(f"Data: {data}")
+      path = data[1]
+      print(f"path: {path}")
+      path_vals: list[bytes] = path.split(b"/")
+      print(f"path vals: {path_vals}")
 
-            elif b"user-agent" in path_vals[1]:
-                print("Hit user agent")
-                if b"User-Agent" in data[3]:
-                    content = data[4].decode()
-                    content = content.split('\r')
-                    content = content[0]
-                    print (f"content: {content}")
-                    conn.send(f"HTTP/1.1 200 Ok\r\nContent-Type: text/plain\r\nContent-Length:{len(content)}\r\n\r\n{content}".encode())
-                else:
-                    conn.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
-            else:
-                conn.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+      if path == b"/":
+        conn.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+
+      elif b"echo" in path_vals[1]:
+        content = f"{path_vals[2].decode()}/{path_vals[3].decode()}"
+        print(f"content: {content}")
+        conn.send(f"HTTP/1.1 200 Ok\r\nContent-Type: text/plain\r\nContent-Length:{len(content)}\r\n\r\n{content}".encode())
+
+      elif b"user-agent" in path_vals[1]:
+        print("Hit user agent")
+        if b"User-Agent" in data[3]:
+          content = data[4].decode()
+          content = content.split('\r')
+          content = content[0]
+          print(f"content: {content}")
+          conn.send(f"HTTP/1.1 200 Ok\r\nContent-Type: text/plain\r\nContent-Length:{len(content)}\r\n\r\n{content}".encode())
+        else:
+          conn.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+      else:
+        conn.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+    except ConnectionError:
+      print(f"Client {addr} disconnected unexpectedly")
+
+
 if __name__ == "__main__":
-    main()
+  server_socket = socket.create_server((HOST, PORT), reuse_port=True)
+  server(server_socket)
