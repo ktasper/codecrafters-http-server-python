cd9ea1eaf7eafbe5c84bafce5c3d3a94b4c98bc6
Karl Webster <19327709+ktasper@users.noreply.github.com>
2024-04-20 10:20:24 +0100

    Test

--- app/main.py
+++ app/main.py
@@ -43,36 +43,37 @@ def handle_client(conn):
       path_vals: list[bytes] = path.split(b"/")
       print(f"path vals: {path_vals}")
 
-      if path == b"/":
-        conn.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+      if data[0].decode() == "GET":
+        if path == b"/":
+          conn.send("HTTP/1.1 200 OK\r\n\r\n".encode())
 
-      elif b"echo" in path_vals[1]:
-        content = f"{path_vals[2].decode()}/{path_vals[3].decode()}"
-        print(f"content: {content}")
-        conn.send(f"HTTP/1.1 200 Ok\r\nContent-Type: text/plain\r\nContent-Length:{len(content)}\r\n\r\n{content}".encode())
-
-      elif b"files" in path_vals[1]:
-        file_name = path_vals[2].decode()
-        print (f"File name: {file_name}")
-        if not os.path.exists(file_name):
-          conn.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
-        with open(file_name, 'r') as f:
-          content = f.read()
-          print (f"Content \n: {content}")
-          conn.send(f"HTTP/1.1 200 Ok\r\nContent-Type: application/octet-stream\r\nContent-Length:{len(content)}\r\n\r\n{content}".encode())
-
-      elif b"user-agent" in path_vals[1]:
-        print("Hit user agent")
-        if b"User-Agent" in data[3]:
-          content = data[4].decode()
-          content = content.split('\r')
-          content = content[0]
+        elif b"echo" in path_vals[1]:
+          content = f"{path_vals[2].decode()}/{path_vals[3].decode()}"
           print(f"content: {content}")
           conn.send(f"HTTP/1.1 200 Ok\r\nContent-Type: text/plain\r\nContent-Length:{len(content)}\r\n\r\n{content}".encode())
+
+        elif b"files" in path_vals[1]:
+          file_name = path_vals[2].decode()
+          print (f"File name: {file_name}")
+          if not os.path.exists(file_name):
+            conn.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+          with open(file_name, 'r') as f:
+            content = f.read()
+            print (f"Content \n: {content}")
+            conn.send(f"HTTP/1.1 200 Ok\r\nContent-Type: application/octet-stream\r\nContent-Length:{len(content)}\r\n\r\n{content}".encode())
+
+        elif b"user-agent" in path_vals[1]:
+          print("Hit user agent")
+          if b"User-Agent" in data[3]:
+            content = data[4].decode()
+            content = content.split('\r')
+            content = content[0]
+            print(f"content: {content}")
+            conn.send(f"HTTP/1.1 200 Ok\r\nContent-Type: text/plain\r\nContent-Length:{len(content)}\r\n\r\n{content}".encode())
+          else:
+            conn.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
         else:
           conn.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
-      else:
-        conn.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
     except ConnectionError:
       print(f"Client disconnected unexpectedly")
 
