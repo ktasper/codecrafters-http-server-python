98a39d72995134de0faa866817c20ba2dfc2ef49
Karl Webster <19327709+ktasper@users.noreply.github.com>
2024-04-20 10:24:45 +0100

    Test

--- app/main.py
+++ app/main.py
@@ -35,7 +35,7 @@ def handle_client(conn):
         if b"files" in path_vals[1]:
           file_name = path_vals[2].decode()
           print (f"File name: {file_name}")
-          print (f"Conn Data: {conn_data}")
+          print (f"Conn Data: {conn_data.split('\r')}")
 
 
       if data[0].decode() == "GET":
